e95ff9619333f4bfbf11f16a4872c7f0f0a69365
Tom Kooij <74959713+TommyKooij@users.noreply.github.com>
2026-05-12 13:41:07 +0200

    12/05/2026 2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 import express from 'express';
+import articles from "../data/articles.js"
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', {title: 'Product Listing'});
+  res.render('index', {title: 'Product Listing', articles: articles});
+});
+
+router.get('/articles/:id', (req, res) => {
+  const article = articles.find((a) => a.id === parseInt(req.params.id));
+
+  res.render('article', { title: article.name, article: article });
+});
+
+router.post('/articles', (req, res) => {
+  const { name, body } = req.body;
+
+  const article = {
+    id: Math.floor(Math.random() * 1000000),
+    name,
+    body
+  };
+  articles.push(article);
+
+  res.render('partials/list', {articles})
 });
 
 export default router;
